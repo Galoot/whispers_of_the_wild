ac317f4c3ab4b2aa01b0bf9d01f15468de49a132
Rudi Minty <rudi@onlineintelligence.co.za>
2014-09-08 23:56:52 +0200

    * Main Menu basic layout

--- www/js/index.js
+++ www/js/index.js
@@ -16,10 +16,18 @@ function Controller() {
     this.onDeviceReady = function() {
         $(window).bind("pageshow resize orientationchange", function(e) { // resize page if needed
             maxHeight();
+            alignOptions();
         });
 
         // Initialise my stuff here
         initialiseListeners();
+
+        // Splash screen then main menu
+        window.setTimeout(
+            function() {
+                location.href = "#menu";
+            },
+            3000);
     };
 
     var initialiseListeners = function() {
@@ -31,22 +39,45 @@ function Controller() {
 //        );
     };
 
+    var alignOptions = function() {
+        var option_size = 100;
+        var option_padding = 15;
+        var top_margin_start = 50;
+        $(".options-row1").css("margin-top", (top_margin_start) + "px");
+        $(".options-row2").css("margin-top", (top_margin_start + option_size + option_padding) + "px");
+        $(".options-row3").css("margin-top", (top_margin_start + option_size + option_padding + option_size + option_padding) + "px");
+
+        var left_margin_centre = -50;
+
+        $("[data-role='option'].far-left").css("margin-left", (left_margin_centre - option_size - option_padding) + "px");
+        $("[data-role='option'].left").css("margin-left", (left_margin_centre - (option_size/2) - (option_padding/2)) + "px");
+        $("[data-role='option'].centre").css("margin-left", (left_margin_centre + 0) + "px");
+        $("[data-role='option'].right").css("margin-left", (left_margin_centre + (option_size/2) + (option_padding/2)) + "px");
+        $("[data-role='option'].far-right").css("margin-left", (left_margin_centre + option_size + option_padding) + "px");
+    };
+
     var maxHeight = function() {
-        var h = $("#header").outerHeight(true);
-        var f = $("#footer").outerHeight(true);
+        var header = $("[data-role='header']");
+        var header_oh = header.height() > 0 ? header.outerHeight(true) : 0;
 
-        var c = $("#content");
+        var footer = $("[data-role='footer']");
+        var footer_oh = footer.height() > 0 ? footer.outerHeight(true) : 0;
 
-        var w = $(window).height();
+        var content = $("[data-role='content']");
 
-        var c_h = c.height();
-        var c_oh = c.outerHeight(true);
-        var c_new = w - h - f - c_oh + c_h;
-        var total = h + f + c_oh;
-        if (c_h < c.get(0).scrollHeight) {
-            c.height(c.get(0).scrollHeight);
-        } else {
-            c.height(c_new);
-        }
+        var window_h = $(window).height();
+
+        var content_h = content.height();
+        var content_oh = content_h > 0 ? content.outerHeight(true) : 0;
+
+        var c_new = window_h - header_oh - footer_oh - content_oh + content_h;
+        content.height(c_new);
+
+//        var total = header_oh + footer_oh + content_oh;
+//        if (content_h < content.get(0).scrollHeight) {
+//            content.height(content.get(0).scrollHeight);
+//        } else {
+//            content.height(c_new);
+//        }
     };
 }
